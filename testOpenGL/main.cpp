0347e35d9b9e25f6544f24f4a6f375654227e2a0
Yuriy Sidorov <syvwgl@gmail.com>
2021-06-18 21:06:54 +0300

    rectangle drawing using elements arrays buffer

--- testOpenGL/main.cpp
+++ testOpenGL/main.cpp
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <vector>
+#include <array>
 #include <fstream>
 #include <string>
 
@@ -34,16 +35,34 @@ int main()
 
 	setUpWindow( window );
 
-	//hardcoded triangle
-	float vertices[] = {
-		-0.5f, -0.5f, 0.0f,
-		 0.5f, -0.5f, 0.0f,
-		 0.0f,  0.5f, 0.0f
+	//hardcoded triangles
+	std::array<float, 12> vertices = {
+	 0.5f,  0.5f, 0.0f,  // top right
+	 0.5f, -0.5f, 0.0f,  // bottom right
+	-0.5f, -0.5f, 0.0f,  // bottom left
+	-0.5f,  0.5f, 0.0f   // top left 
 	};
+	std::array<unsigned int, 6> indices = {  // note that we start from 0!
+		0, 1, 3,   // first triangle
+		1, 2, 3    // second triangle
+	};
+	
 	unsigned int VBO;
+	unsigned int VAO;
+	unsigned int EBO;
+	glGenVertexArrays( 1, &VAO );
+	glBindVertexArray( VAO );
+	//set vertexes and copy to VRAM
 	glGenBuffers( 1, &VBO );
 	glBindBuffer( GL_ARRAY_BUFFER, VBO );
-	glBufferData( GL_ARRAY_BUFFER, sizeof( vertices ), vertices, GL_STATIC_DRAW );
+	glBufferData( GL_ARRAY_BUFFER, sizeof( vertices ), vertices.data(), GL_STATIC_DRAW );
+	//set index array and copy to VRAM
+	glGenBuffers( 1, &EBO );
+	glBindBuffer( GL_ELEMENT_ARRAY_BUFFER, EBO );
+	glBufferData( GL_ELEMENT_ARRAY_BUFFER, sizeof( indices ), indices.data(), GL_STATIC_DRAW );
+	//set vertex atributes pointers
+	glVertexAttribPointer( 0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof( float ), ( void* )0 );
+	glEnableVertexAttribArray( 0 );
 
 	//main loop
 	while ( !glfwWindowShouldClose( window ) ) {
@@ -52,6 +71,8 @@ int main()
 		
 		//rendering commands
 		glClear( GL_COLOR_BUFFER_BIT );
+		glBindVertexArray( VAO );
+		glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, 0 );
 
 		//check and call events and swap the buffers
 		glfwPollEvents();
