b5772b7ff3eb24ec815d07c385b96bfaaee963ba
Yuriy Sidorov <syvwgl@gmail.com>
2021-06-18 20:10:05 +0300

    refactor main to use try...catch

--- testOpenGL/main.cpp
+++ testOpenGL/main.cpp
@@ -13,31 +13,26 @@ const unsigned int BUFFER_SIZE = 512;
 void framebuffer_size_callback( GLFWwindow* window, int width, int height );
 void processInput( GLFWwindow* window );
 
+void initializeWindow(GLFWwindow** pWindow) noexcept(false);
+void setUpWindow( GLFWwindow* window ) noexcept;
 void initializeShaders() noexcept(false);
 
 int main() 
 {
-	//initialize and set GLFW	
-	glfwInit();
-	glfwWindowHint( GLFW_CONTEXT_VERSION_MAJOR, 3 );
-	glfwWindowHint( GLFW_CONTEXT_VERSION_MINOR, 3 );
-	glfwWindowHint( GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE );
+	GLFWwindow* window = nullptr;	
 
-	//create window
-	GLFWwindow* window = glfwCreateWindow( 800, 600, "OpenGL TESTS", NULL, NULL );
-	if ( !window ) {
-		std::cerr << "Falied to create GLFW window!\n";
+	try {
+		initializeWindow( &window );
+		initializeShaders();
+	}
+	catch ( std::exception& e ) {
+		std::cerr << e.what() << std::endl;
+		std::cerr << "Class: " << typeid( e ).name() << std::endl;
 		glfwTerminate();
 		return -1;
 	}
-	glfwMakeContextCurrent( window );
 
-	//initialize GLAD
-	if ( !gladLoadGLLoader( ( GLADloadproc )glfwGetProcAddress ) ) {
-		std::cerr << "Failed to initialize GLAD!\n";
-		return -1;
-	}
-	glfwSetFramebufferSizeCallback( window, framebuffer_size_callback );
+	setUpWindow( window );
 
 	//hardcoded triangle
 	float vertices[] = {
@@ -50,18 +45,6 @@ int main()
 	glBindBuffer( GL_ARRAY_BUFFER, VBO );
 	glBufferData( GL_ARRAY_BUFFER, sizeof( vertices ), vertices, GL_STATIC_DRAW );
 
-	//intialize shaders
-	try {
-		initializeShaders();
-	}
-	catch ( std::exception& e ) {
-		std::cerr << e.what() << std::endl;
-		std::cerr << "Class: " << typeid( e ).name() << std::endl;
-		glfwTerminate();
-		return -1;
-	}
-
-	glClearColor( 0.5f, 0.5f, 0.5f, 1.f );
 	//main loop
 	while ( !glfwWindowShouldClose( window ) ) {
 		//input
@@ -88,6 +71,32 @@ void processInput( GLFWwindow* window )
 {
 	if ( glfwGetKey( window, GLFW_KEY_ESCAPE ) == GLFW_PRESS )
 		glfwSetWindowShouldClose( window, true );
+}
+
+void initializeWindow( GLFWwindow** pWindow ) noexcept( false )
+{
+	//initialize and set GLFW	
+	glfwInit();
+	glfwWindowHint( GLFW_CONTEXT_VERSION_MAJOR, 3 );
+	glfwWindowHint( GLFW_CONTEXT_VERSION_MINOR, 3 );
+	glfwWindowHint( GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE );
+
+	//create window
+	*pWindow = glfwCreateWindow( 800, 600, "OpenGL TESTS", NULL, NULL );
+	if ( !*pWindow )
+		throw ( std::runtime_error( std::string( "INIT::GLFW::Falied to create GLFW window!") ) );
+	glfwMakeContextCurrent( *pWindow );
+
+	//initialize GLAD
+	if ( !gladLoadGLLoader( ( GLADloadproc )glfwGetProcAddress ) )
+		throw ( std::runtime_error( std::string( "INIT::GLAD::Failed to initialize GLAD!" ) ) );
+
+}
+
+void setUpWindow( GLFWwindow* window ) noexcept
+{
+	glfwSetFramebufferSizeCallback( window, framebuffer_size_callback );
+	glClearColor( 0.5f, 0.5f, 0.5f, 1.f );
 }
 
 
